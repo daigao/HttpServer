16b77043c962ce04aff74b2d713fb911e2c948b1
daigao <daigao@LAPTOP-UQ84L1LU>
2024-07-07 15:51:08 +0800

    完善写法

--- HttpServer/Program.cs
+++ HttpServer/Program.cs
@@ -6,14 +6,17 @@ namespace HttpServer
     {
         static void Main(string[] args)
         {
-            HttpServer httpServer = new HttpServer();
-            httpServer.Bind(null) ;
-            httpServer.RequestMessageCallback += (context) => {
-                Console.WriteLine($"{context.Method} {context.FullUri}");
-                return new ResponseMessage("你好服务端");
-            };
-            httpServer.BeginAccept();
-            Console.ReadKey();
+            using (HttpServer httpServer = new HttpServer())
+            {
+                httpServer.Bind(null);
+                httpServer.RequestMessageCallback += (context) =>
+                {
+                    Console.WriteLine($"{context.Method} {context.FullUri}");
+                    return new ResponseMessage("你好服务端");
+                };
+                httpServer.BeginAccept();
+                Console.ReadKey();
+            }
         }
     }
 }
